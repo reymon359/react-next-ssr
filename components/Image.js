2b954c7beaebe3fdef94597e16e793d03c577c07
ramon morcillo <ramonmorcillocascales@gmail.com>
2020-03-27 07:31:50 +0100

    feat(about.js): add a React image component to the about page

---
+++ components/Image.js
@@ -0,0 +1,5 @@
+const Image = () => (
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Cam%C3%A9l%C3%A9on_Madagascar_02.jpg/220px-Cam%C3%A9l%C3%A9on_Madagascar_02.jpg" alt=""/>
+); 
+
+export default Image;
--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from 'react';
 import Link from 'next/link';
+import Image from '../components/Image';
 
 const About = () => {
-  return (
-    <div style={{fontSize: '20px', color: 'blue'}}>
-      <h1>About</h1>
-      <Link href='/'>
-        <button>Back</button>
-      </Link>
-      <p>This is the about page</p>
-    </div>
-  )
-}
+	return (
+		<div style={{ fontSize: '20px', color: 'blue' }}>
+			<h1>About</h1>
+			<Link href="/">
+				<button>Back</button>
+			</Link>
+			<Image />
+			<p>This is the about page</p>
+		</div>
+	);
+};
 
-export default About
+export default About;
